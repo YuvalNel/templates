5344105ac73f4525e3d0549306592e413f55b8d7
Hever Farber <hever.farber@gmail.com>
2024-01-18 14:22:45 +0200

    pr-module

--- tests/null-test/main.tf
+++ tests/null-test/main.tf
@@ -4,8 +4,19 @@ resource "local_file" "test" {
 }
 
 resource "time_sleep" "wait_30_seconds" {
-  create_duration = "5s"
-  destroy_duration = "10s"
+  create_duration = "1s"
+  destroy_duration = "2s"
+}
+
+resource "null_resource" "hever" {
+  count = var.counter
+  triggers = {
+    test = "hever"
+  }
+}
+
+variable "counter" {
+    default = 0
 }
 
 variable "instances" {
